7fcba0223f6fcc344bab3f40a9bea08a322e94b1
sryzhkov <sryzhkov@neoflex.ru>
2023-01-17 11:54:53 +0300

    make CharInfo and CharList components

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -9,7 +9,6 @@ import mjolnir from "../../resources/img/mjolnir.png";
 class RandomChar extends Component {
   constructor(props) {
     super(props);
-    this.updateChar();
   }
 
   state = {
@@ -20,16 +19,33 @@ class RandomChar extends Component {
 
   marvelService = new MarvelService();
 
-  onCharLoaded = (char) => {
-    this.setState({ char, loading: false });
-  };
+  componentDidMount() {
+    this.updateChar();
+    // this.timerID = setInterval(this.updateChar, 3000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    this.onCharLoading();
     this.marvelService
       .getCharacter(id)
       .then(this.onCharLoaded)
       .catch(this.onError);
+    console.log(this.state.error);
+  };
+
+  onCharLoading = () => {
+    this.setState({
+      loading: true,
+    });
+  };
+
+  onCharLoaded = (char) => {
+    this.setState({ char, loading: false, error: false });
   };
 
   onError = () => {
@@ -57,7 +73,7 @@ class RandomChar extends Component {
             Do you want to get to know him better?
           </p>
           <p className="randomchar__title">Or choose another one</p>
-          <button className="button button__main">
+          <button className="button button__main" onClick={this.updateChar}>
             <div className="inner">try it</div>
           </button>
           <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -69,10 +85,22 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki } = char;
+  let imgStyle = { objectFit: "cover" };
+  if (
+    thumbnail ===
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+  ) {
+    imgStyle = { objectFit: "unset" };
+  }
 
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <img
+        src={thumbnail}
+        alt="Random character"
+        className="randomchar__img"
+        style={imgStyle}
+      />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">{description}</p>
